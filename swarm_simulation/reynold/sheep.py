52d02d2003dde00afc6df83d19e4c680ac34f60d
Royce <roycel@stud.ntnu.no>
2022-12-01 15:25:48 +0100

    .

--- swarm_simulation/reynold/sheep.py
+++ swarm_simulation/reynold/sheep.py
@@ -36,9 +36,6 @@ class Sheep:
         canvas.create_oval(x0, y0, x1, y1, fill='white', tags=self.id)
         canvas.create_text(self.position[0], self.position[1], text=self.id[-1], tags=self.id)
     
-    # def get_position(self):
-    #     return (self.position[0], self.position[1])
-    
     def update_sheep(self):
         self.position = np.add(self.position, self.velocity)
         self.velocity = np.add(self.velocity, self.acceleration)
@@ -52,7 +49,6 @@ class Sheep:
     
 
     def main_sheep(self, list_of_sheep, canvas, list_of_drones):
-
         for drone in list_of_drones:
             if np.linalg.norm(drone.position - self.position) < self.desired_separation_drones:
                 self.max_speed = 1.7
@@ -65,7 +61,9 @@ class Sheep:
         v2 = self.separation(list_of_sheep)
         v2_2 = self.drone_separation(list_of_drones)
         v3 = self.alignment(list_of_sheep)
-        self.acceleration += v1 + v2 + v2_2 + v3
+
+        self.acceleration = np.add(self.acceleration, v1, v2)
+        self.acceleration = np.add(self.acceleration, v2_2, v3)
 
         canvas.delete(self.id)
         self.draw_sheep(canvas)
@@ -78,14 +76,14 @@ class Sheep:
         center_of_mass = np.zeros(2)
         for boid in nearest_sheep:
             if np.linalg.norm(boid.position - self.position) < self.horizon:
-                center_of_mass += boid.position
+                center_of_mass = np.add(center_of_mass, boid.position)
                 total += 1
         if total > 0:
             center_of_mass /= total
             vec_to_com = center_of_mass - self.position
             if np.linalg.norm(vec_to_com) > 0:
                 vec_to_com = (vec_to_com / np.linalg.norm(vec_to_com)) * self.max_speed
-            steering = vec_to_com - self.velocity
+            steering = np.subtract(vec_to_com, self.velocity)
             if np.linalg.norm(steering)> self.max_force:
                 steering = (steering /np.linalg.norm(steering)) * self.max_force
 
@@ -101,7 +99,7 @@ class Sheep:
             if (self.position[0] != boid.position[0] and self.position[1] != boid.position[1])and distance < self.horizon:
                 diff = self.position - boid.position
                 diff = (diff[0]/distance, diff[1]/distance)
-                avg_vector += diff
+                avg_vector = np.add(avg_vector, diff)
                 total += 1
         if total > 0:
             avg_vector /= total
@@ -123,25 +121,16 @@ class Sheep:
             if np.linalg.norm(drone.position - self.position) < self.desired_separation_drones:
                 diff = self.position - drone.position
                 diff /= np.linalg.norm(drone.position - self.position)
-                avg_vector += diff
+                avg_vector = np.add(avg_vector, diff)
                 total += 1
         if total > 0:
             avg_vector /= total
             if np.linalg.norm(steering) > 0:
                 avg_vector = (avg_vector / np.linalg.norm(steering)) * self.max_speed
-            steering = avg_vector - self.velocity
+            steering = np.subtract(avg_vector, self.velocity)
             if np.linalg.norm(steering) > self.max_force:
                 steering = (steering /np.linalg.norm(steering)) * self.max_force
-        
-        
-        # c = np.zeros(2)
-        
-        # for drone in nearest_drone:
-        #     if ((np.linalg.norm(drone.position - self.position) < self.desired_separation_drones)
-        #             & (drone != self)):
-        #         c -= (drone.position - self.position)*(self.separation_weight_drones/100)
-        #         self.velocity = drone.velocity
-        # return c
+
         return steering
 
     
@@ -152,12 +141,11 @@ class Sheep:
         avg_vector = np.zeros(2)
         for sheep in nearest_sheep:
             if np.linalg.norm(sheep.position - self.position) < self.horizon:
-                avg_vector += sheep.velocity
+                avg_vector = np.add(avg_vector, sheep.velocity)
                 N += 1
         if N > 0:
             avg_vector /= N
             avg_vector = (avg_vector / np.linalg.norm(avg_vector)) * self.max_speed
-            steering = avg_vector - self.velocity
+            steering = np.subtract(avg_vector, self.velocity)
 
         return steering
-
