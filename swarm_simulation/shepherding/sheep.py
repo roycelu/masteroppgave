662870c5e2b3ca1e7deaebfd303df95c9cec91da
mcbelsvi <mcbelsvi@stud.ntnu.no>
2023-02-01 14:40:40 +0100

    started occlusion

--- swarm_simulation/shepherding/sheep.py
+++ swarm_simulation/shepherding/sheep.py
@@ -5,14 +5,14 @@ import numpy as np
 Code inspired by https://betterprogramming.pub/boids-simulating-birds-flock-behavior-in-python-9fff99375118
 """
 
-SIZE = 10
+SIZE = 2
 MAX_SPEED = 1.5
 MAX_FORCE = 0.2
-PERCEPTION = 100
+PERCEPTION = 70
 A_WEIGHT = 10
 C_WEIGHT = 30
-S_WEIGHT = 50
-DRONE_SEPARATION = 50  #
+S_WEIGHT = 100
+DRONE_SEPARATION = 100  #
 
 
 class Sheep:
@@ -46,12 +46,12 @@ class Sheep:
         self.velocity += self.acceleration
         if self.velocity.magnitude() > MAX_SPEED:
             self.velocity = self.velocity / self.velocity.magnitude() * MAX_SPEED
-        self.acceleration = pygame.Vector2(0, 0)
+        #self.acceleration = pygame.Vector2(0, 0)
 
-    def move(self, goal, sheeps, drones):
-        alignment = self.alignment(sheeps)
-        cohesion = self.cohesion(sheeps)
-        separation = self.separation(sheeps)
+    def move(self, goal, sheep, drones):
+        alignment = self.alignment(sheep)
+        cohesion = self.cohesion(sheep)
+        separation = self.separation(sheep)
         escape = self.escape(drones)  # Move away from predators
 
         self.acceleration += alignment * A_WEIGHT
@@ -65,8 +65,70 @@ class Sheep:
             # print("{id} reached the goal".format(id="sheep" + str(self.id)))
             pass
 
+
+    def separation(self, sheep):
+        total = pygame.Vector2(0, 0)
+        for s in sheep:
+            distance = self.position-s.position
+            if self != s and distance.magnitude() < PERCEPTION:
+                separation = distance / (np.linalg.norm(self.position-s.position))**2
+                total += separation
+        total /= len(sheep)
+        return total
+    
+    def alignment(self, sheep):
+        total = pygame.Vector2(0, 0)
+        for s in sheep:
+            distance = self.position-s.position
+            if self != s and distance.magnitude() < PERCEPTION:
+                alignment = s.velocity / np.linalg.norm(s.velocity)
+                total += alignment
+        total /= len(sheep)
+        return total
+
+    def cohesion(self, sheep):
+        total = pygame.Vector2(0, 0)
+        for s in sheep:
+            distance = self.position-s.position
+            if self != s and distance.magnitude() < PERCEPTION:
+                cohesion = distance / np.linalg.norm(self.position-s.position)
+                total += cohesion
+        total /= len(sheep)
+        total *= (-1)
+        return total
+    
+    def escape(self, drones):
+        total = pygame.Vector2(0, 0)
+        for drone in drones:
+            distance = self.position-drone.position
+            if distance.magnitude() < PERCEPTION:
+                separation = distance / (np.linalg.norm(self.position-drone.position))**3
+                total += separation
+        total /= len(drones)
+        return total
+
+    """
+    def cohesion(self, sheeps):
+            #The sheep move together as a flock
+            total = 0
+            steering = pygame.Vector2(0, 0)
+            center_of_mass = pygame.Vector2(0, 0)
+            for sheep in sheeps:
+                if (sheep.position - self.position).magnitude() < PERCEPTION:
+                    center_of_mass += sheep.position
+                    total += 1
+            if total > 0:
+                center_of_mass /= total
+                vector = center_of_mass - self.position
+                if vector.magnitude() > 0:
+                    vector = (vector / vector.magnitude()) * MAX_SPEED
+                steering = vector - self.velocity
+                if steering.magnitude() > MAX_FORCE:
+                    steering = (steering / steering.magnitude()) * MAX_FORCE
+            return steering
+
     def alignment(self, sheeps):
-        """The sheep orient themselves toward each other"""
+        #The sheep orient themselves toward each other
         total = 0
         steering = pygame.Vector2(0, 0)
         average = pygame.Vector2(0, 0)
@@ -80,46 +142,29 @@ class Sheep:
             steering = average - self.velocity
         return steering
 
-    def cohesion(self, sheeps):
-        """The sheep move together as a flock"""
-        total = 0
-        steering = pygame.Vector2(0, 0)
-        center_of_mass = pygame.Vector2(0, 0)
-        for sheep in sheeps:
-            if (sheep.position - self.position).magnitude() < PERCEPTION:
-                center_of_mass += sheep.position
-                total += 1
-        if total > 0:
-            center_of_mass /= total
-            vector = center_of_mass - self.position
-            if vector.magnitude() > 0:
-                vector = (vector / vector.magnitude()) * MAX_SPEED
-            steering = vector - self.velocity
-            if steering.magnitude() > MAX_FORCE:
-                steering = (steering / steering.magnitude()) * MAX_FORCE
-        return steering
-
     def separation(self, sheeps):
-        """The sheep avoid colliding with each other"""
-        total = 0
-        steering = pygame.Vector2(0, 0)
-        average = pygame.Vector2(0, 0)
-        for sheep in sheeps:
-            distance = sheep.position - self.position
-            if self != sheep and distance.magnitude() < PERCEPTION:
-                average += (self.position - sheep.position) / distance.magnitude()
-                total += 1
-        if total > 0:
-            average /= total
-            if steering.magnitude() > 0:
-                average = (average / steering.magnitude()) * MAX_SPEED
-            steering = average - self.velocity
-            if steering.magnitude() > MAX_FORCE:
-                steering = (steering / steering.magnitude()) * MAX_FORCE
-        return steering
+            #The sheep avoid colliding with each other
+            total = 0
+            steering = pygame.Vector2(0, 0)
+            average = pygame.Vector2(0, 0)
+            for sheep in sheeps:
+                distance = sheep.position - self.position
+                if self != sheep and distance.magnitude() < PERCEPTION:
+                    average += (self.position - sheep.position) / distance.magnitude()
+                    total += 1
+            if total > 0:
+                average /= total
+                if steering.magnitude() > 0:
+                    average = (average / steering.magnitude()) * MAX_SPEED
+                steering = average - self.velocity
+                if steering.magnitude() > MAX_FORCE:
+                    steering = (steering / steering.magnitude()) * MAX_FORCE
+            return steering
+
+    
 
     def escape(self, drones):
-        """The sheep flee from the predators (custom)"""
+        #The sheep flee from the predators (custom)
         steering = pygame.Vector2(0, 0)
         for drone in drones:
             distance = drone.position - self.position
@@ -127,3 +172,4 @@ class Sheep:
                 # steering -= distance
                 steering = distance / distance.magnitude()
         return steering
+    """
