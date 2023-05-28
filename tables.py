a7b7406ae36875f401ed2695ca9541fe928e7784
Royce <roycel@stud.ntnu.no>
2023-05-28 11:03:50 +0200

    ryddet i koden til for å lage tabeller med resultater

--- tables.py
+++ tables.py
@@ -1,29 +1,29 @@
 import os
 import numpy as np
 import pandas as pd
-import scipy
+from scipy import stats
 from constants import *
 
 PATH = "./final"
+PATH_OUR = "./final/our"
 DRONENAMES = {'our': 'Massesenter', 'polygon': 'Polygon', 'circle': 'Sirkel', 'v': 'V'}
 DRONENAMES_OUR = {'com': 'Massesenter', 'v_polygon': 'V-polygon'}
-PERCEPTION = 20
-ANGLE = 20
+BEST_ANGLE = 20
 
 
 def create_csv_angle(test, dronetypes, perception, testtypes = TESTTYPES, angles = ANGLES):
+    file_columns = ["Testtype","Vinkel","Gjetetid","Suksessrate","Oppsamlingstid","Drivetid"]
+
     for testtype in testtypes:
+        # Create a new table
+        columns = [file_columns[0], "Dronetype", file_columns[1], file_columns[3], "Gjennomsnitt", "Standardavvik"]
+        df = pd.DataFrame(columns=columns[1:]) # OBS! 'Testtype' no longer in use
+
         for drone in dronetypes:
-
             # Read from CSV file for a drone
-            df_results = pd.read_csv("{path}/{d}_{p}.csv".format(path=PATH, d=drone, p=perception))
-            file_columns = ["Testtype","Vinkel","Gjetetid","Suksessrate","Oppsamlingstid","Drivetid"]
+            df_results = pd.read_csv("{path}/{d}_{p}.csv".format(path=PATH_OUR, d=drone, p=perception))
 
-            # Create a new table
-            columns = [file_columns[0], "Dronetype", file_columns[1], file_columns[3], "Gjennomsnitt", "Standardavvik"]
-            df = pd.DataFrame(columns=columns[1:]) # OBS! 'Testtype' no longer in use
-        
-            for angle in angles:                
+            for angle in angles:           
                 # Retrieve the correct column by conditions from the CSV file
                 data = df_results[(df_results[file_columns[0]] == testtype) & (df_results[file_columns[1]] == angle)]
                 
@@ -34,32 +34,30 @@ def create_csv_angle(test, dronetypes, perception, testtypes = TESTTYPES, angles
                 data = data[(data[file_columns[3]] == 100)]
                 average = data[file_columns[2]].mean().round(2)
                 std = data[file_columns[2]].std().round(2)
-
-                print(testtype, drone, angle, success, average, std)
                 
                 # Add the values to the table
-                df = df.append({columns[1]: DRONENAMES_OUR[drone], columns[2]: angle, columns[3]: success, columns[4]: average, columns[5]: std}, ignore_index=True)
+                df.loc[len(df), df.columns] = DRONENAMES_OUR[drone], angle, success, average, std
 
-            # Append the table row to a CSV file
-            filename = "{}/overview/{}_{}{}.csv".format(PATH, test, testtype, perception)
-            df.to_csv(filename, mode='a', index=False, header=False  if os.path.isfile(filename) else True)
+        # Save the dataframe as a CSV file
+        filename = "{}/overview/{}_{}{}.csv".format(PATH_OUR, test, testtype, perception)
+        df.to_csv(filename, index=False)
 
 
 def create_csv(test, dronetypes, perceptions = PERCEPTIONS, testtypes = TESTTYPES):
+    file_columns = ["Testtype","Gjetetid","Suksessrate","Oppsamlingstid","Drivetid"]
+
     for testtype in testtypes:
+        # Create a new table
+        columns = [file_columns[0], "Dronetype", "Synsrekkevidde", file_columns[2], "Gjennomsnitt", "Standardavvik"]
+        df = pd.DataFrame(columns=columns[1:]) # OBS! 'Testtype' no longer in use
+
         for drone in dronetypes:
             for perception in perceptions:
                 # Read from CSV file for a drone and a perception
                 if drone == 'our':
-                    df_results = pd.read_csv("{path}/{d}_{p}_{a}.csv".format(path=PATH, d=drone, p=perception, a=ANGLE))
+                    df_results = pd.read_csv("{path}/{d}_{p}_{a}.csv".format(path=PATH, d=drone, p=perception, a=BEST_ANGLE))
                 else:
                     df_results = pd.read_csv("{path}/{d}_{p}.csv".format(path=PATH, d=drone, p=perception))
-
-                file_columns = ["Testtype","Gjetetid","Suksessrate","Oppsamlingstid","Drivetid"]
-
-                # Create a new table
-                columns = [file_columns[0], "Dronetype", "Synsrekkevidde", file_columns[2], "Gjennomsnitt", "Standardavvik"]
-                df = pd.DataFrame(columns=columns[1:]) # OBS! 'Testtype' no longer in use
 
                 # Retrieve the correct column by conditions from the CSV file
                 data = df_results[(df_results[file_columns[0]] == testtype)]
@@ -71,11 +69,11 @@ def create_csv(test, dronetypes, perceptions = PERCEPTIONS, testtypes = TESTTYPE
                 std = round(data[file_columns[1]].std(), 2)
                 
                 # Add the values to the table
-                df = df.append({columns[1]: DRONENAMES[drone], columns[2]: perception, columns[3]: success, columns[4]: average, columns[5]: std}, ignore_index=True)
+                df.loc[len(df), df.columns] = DRONENAMES[drone], perception, success, average, std
             
-                # Append the table row to a CSV file
-                filename = "{}/overview/{}_{}.csv".format(PATH, test, testtype)
-                df.to_csv(filename, mode='a', index=False, header=False  if os.path.isfile(filename) else True)
+        # Save the dataframe as a CSV file
+        filename = "{}/overview/{}_{}.csv".format(PATH, test, testtype)
+        df.to_csv(filename, index=False)
 
 def p_value(path):
     columns = ['Testtype', 'Dronetype', 'Synsrekkevidde', 'P-verdi']
@@ -99,27 +97,36 @@ def p_value(path):
 
                 # Only calculate p-value if the value is not NAN
                 if not np.isnan(mean) and not np.isnan(std):
-                    stat, p_value = scipy.stats.ttest_ind_from_stats(our_mean, our_std, NO_SIMULATIONS, mean, std, NO_SIMULATIONS)
+                    stat, p_value = stats.ttest_ind_from_stats(our_mean, our_std, NO_SIMULATIONS, mean, std, NO_SIMULATIONS)
                     # Add the table row to a CSV file
                     df.loc[len(df), df.columns] = testtype, drone, perception, round(p_value, 4)
+    
+    # Save the dataframe as a CSV file
     filename = "{}/overview/p-value.csv".format(PATH)
     df.to_csv(filename, index=False)
 
-def main():
-    # NB! Remember to delete the CSV-files in the PATH before running the code!
 
+def main():
+    # Make sure that the folders exist
     path = PATH + '/overview'
     if not os.path.exists(path):
         os.mkdir(path)
 
-    # # Existing methods
-    # create_csv("existing", DRONETYPES[1:])
+    path_our = PATH_OUR + '/overview'
+    if not os.path.exists(path_our):
+        os.mkdir(path_our)
 
-    # # Our methods
-    # create_csv_angle("our", OUR_DRONETYPES, PERCEPTION)
+    # Create tables for the different methods
 
-    # # All methods
-    # create_csv("all", DRONETYPES)
+    # Existing methods
+    create_csv("existing", DRONETYPES[1:])
+
+    # Our methods
+    for perception in PERCEPTIONS:
+        create_csv_angle("our", OUR_DRONETYPES, perception)
+
+    # All methods
+    create_csv("all", DRONETYPES)
     p_value(path)
 
 if __name__ == "__main__":
